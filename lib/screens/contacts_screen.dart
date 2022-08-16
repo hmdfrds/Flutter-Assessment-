b08702c183f223ca715b0d8d190c3830f3e70728
hmdfrds <hmdfrds1337@gmail.com>
2022-08-16 15:48:55 +0800

    done timeago and sort

--- lib/screens/contacts_screen.dart
+++ lib/screens/contacts_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_assessment/dummy_data.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class ContactPage extends StatefulWidget {
   const ContactPage({Key? key}) : super(key: key);
@@ -9,7 +10,31 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  List<Map<String, String>> contactList = [];
+
+  String convertTime(bool isTimeAgo, String dateTimeInString) {
+    DateTime dateTime = DateTime.parse(dateTimeInString);
+    if (isTimeAgo) {
+      return timeago.format(dateTime);
+    }
+    return dateTimeInString;
+  }
+
   void shareContact() {}
+
+  void sortContactList(List<Map<String, String>> cList) {
+    cList.sort((a, b) {
+      return (b['check-in'] as String).compareTo(a['check-in'] as String);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    contactList.addAll(contacts);
+    sortContactList(contactList);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,13 +45,14 @@ class _ContactPageState extends State<ContactPage> {
         body: ListView.separated(
             itemBuilder: (context, index) {
               return ListTile(
-                title: Text(contacts[index]["user"].toString()),
-                subtitle: Text(contacts[index]["phone"].toString()),
+                title: Text(contactList[index]["user"].toString()),
+                subtitle: Text(contactList[index]["phone"].toString()),
                 trailing: Row(
                   mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text(contacts[index]["check-in"].toString()),
+                    Text(convertTime(
+                        true, contactList[index]["check-in"].toString())),
                     IconButton(
                         onPressed: shareContact, icon: const Icon(Icons.share))
                   ],
